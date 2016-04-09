5ed001f56445558cb0946e6c9618e8093e9a3061
Cody Brouwers <clbrouwers@gmail.com>
2016-04-09 19:54:11 -0400

    Rename to Template

--- config/application.rb
+++ config/application.rb
@@ -6,7 +6,7 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module Thief
+module Template
   class Application < Rails::Application
 
     # Configure sensitive parameters which will be filtered from the log file.
