a9f8ffb62b73b58812943da3665ee956aac33993
Cody Brouwers <clbrouwers@gmail.com>
2016-04-17 03:45:13 -0400

    Add new routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'homepage#index'
+
+  resources :users
+
+  controller :sessions do
+    get     :new,     path: '/login',   as: "login"
+    post    :create,  path: '/login',   as: "create_login"
+    delete  :destroy, path: '/logout',  as: "logout"
+  end
   # Serve websocket cable requests in-process
   # mount ActionCable.server => '/cable'
 end
